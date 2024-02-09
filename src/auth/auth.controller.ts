b6206a7bd14befe6b63d265b78fad130c6576775
celsovcsantos <celsovcsantos@gmail.com>
2024-02-09 18:08:19 -0300

    feat: aula053

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,37 @@
 import { User } from '@/decorators/user.decorator';
+import { FileService } from '@/file/file.service';
 import { AuthGuard } from '@/guards/auth.guard';
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UploadedFiles,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import {
+  FileFieldsInterceptor,
+  FileInterceptor,
+  FilesInterceptor,
+} from '@nestjs/platform-express';
 import { AuthService, IToken } from './auth.service';
 import { AuthForgetDTO } from './dto/authForget.dto';
 import { AuthLoginDTO } from './dto/authLogin.dto';
 import { AuthRegisterDTO } from './dto/authRegister.dto';
 import { AuthResetDTO } from './dto/authReset.dto';
+import { join } from 'path';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly fileService: FileService,
+  ) {}
 
   @Post('login')
   async login(@Body() { email, password }: AuthLoginDTO): Promise<IToken> {
@@ -36,8 +58,69 @@ export class AuthController {
   async me(@User() user: any) {
     return { user };
   }
-  // async me(@Headers('authorization') token: string) {
-  //   //return this.authService.checkToken(token.split(' ')[1]);
-  //   return this.authService.checkToken((token ?? '').replace('Bearer ', ''));
-  // }
+
+  @UseInterceptors(FileInterceptor('file'))
+  @UseGuards(AuthGuard)
+  @Post('photo')
+  async uploadPhoto(
+    @User() user: any,
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new FileTypeValidator({ fileType: 'image/png' }),
+          new MaxFileSizeValidator({
+            maxSize: 1024 * 115,
+            message: 'Arquivo excedeu o tamanho permitido',
+          }),
+        ],
+      }),
+    )
+    photo: Express.Multer.File,
+  ) {
+    const path = join(
+      __dirname,
+      '..',
+      '..',
+      'storage',
+      'photoUser',
+      `photo-${user.id}.${photo.mimetype.split('/')[1]}`,
+    );
+    try {
+      await this.fileService.upload(photo, path);
+    } catch (error) {
+      throw new BadRequestException(`Error to upload photo: ${error}`);
+    }
+    return { sucess: true };
+  }
+
+  @UseInterceptors(FilesInterceptor('files'))
+  @UseGuards(AuthGuard)
+  @Post('files')
+  async uploadFiles(
+    @User() user: any,
+    @UploadedFiles() files: Express.Multer.File[],
+  ) {
+    //console.log(files.length);
+    return files;
+  }
+
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'photo', maxCount: 1 },
+      { name: 'documents', maxCount: 10 },
+    ]),
+  )
+  @UseGuards(AuthGuard)
+  @Post('filesFields')
+  async uploadFilesFields(
+    @User() user: any,
+    @UploadedFiles()
+    files: { photo: Express.Multer.File; documents: Express.Multer.File[] },
+  ) {
+    //console.log(files);
+    //const { photo, documents } = files;
+    //console.log(photo);
+    //console.log(documents);
+    return files; //{ photo, documents };
+  }
 }
